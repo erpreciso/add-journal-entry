1aa8d735dfd84fba9ee12a7b69caf056719c8a5d
erpreciso <trepreciso@gmail.com>
2017-07-07 22:22:50 +0200

    change default folder

--- journal.sh
+++ journal.sh
@@ -1,6 +1,31 @@
-# read date from user input
-echo "insert date mm-dd (2017 is in by default): "
-read mydate
-mydate="2017-$mydate"
+write_journal()
+{
+  # read date from user input
+  echo "insert date mm-dd (2017 is in by default): "
+  read mydate
+  mydate="2017-$mydate"
 
-local
+  # create file if not exist
+  local file="/home/erpreciso/Dropbox/journal/$mydate-journal.txt"
+  if [ ! -f "$file" ] ; then
+    touch "$file"
+  fi
+
+  # ask entry content
+  echo "insert entry content. one line only. if you want more than one line, edit the output file"
+  read content
+
+  # write to file
+  echo "---" >> "$file"
+  echo 'title: "No Title"' >> "$file"
+  echo "date: $mydate" >> "$file"
+  echo "tag: journal" >> "$file"
+  echo "" >> "$file"
+  echo "---" >> "$file"
+  echo "$content" >> "$file"
+}
+
+# execute function
+write_journal
+
+
